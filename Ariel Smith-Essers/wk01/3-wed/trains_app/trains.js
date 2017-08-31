ed0f0f568eb90ff857fffac7b095d4c71d58aac6
arielse <ariel@bottledup.net>
2017-08-31 23:12:14 +1000

    updated to add in single line trips

--- Ariel Smith-Essers/wk01/3-wed/trains_app/trains.js
+++ Ariel Smith-Essers/wk01/3-wed/trains_app/trains.js
@@ -5,37 +5,50 @@ var glenWaverly = ['Flagstaff', 'Melbourne Central', 'Parliament', 'Richmond', '
 var sandringham = ['Southern Cross', 'Richmond', 'South Yarra', 'Prahran', 'Windsor'];
 var lines = [alamein, glenWaverly, sandringham];
 
-var origin;
-var destination;
 var intersection = 'Richmond';
 
-origin = 'Tooroonga';
-destination = 'Glenferrie';
-
-// alamein.slice(origIndex, destIndex + 1);
+var origin = 'Glenferrie';
+var destination = 'Flinders Street';
 
 if (origin === intersection || destination === intersection) {
- console.log(intersection);
+  for (var i = 0; i < lines.length; i++) {
+    if (lines[i].indexOf(origin) !== -1 && lines[i].indexOf(destination) !== -1) {
+      var oneLineTrip = lines[i];
+      var origIndex = oneLineTrip.indexOf(origin);
+      var destIndex = oneLineTrip.indexOf(destination);
+      if (origIndex > destIndex) {
+        oneLineTrip.reverse();
+        var origIndex = oneLineTrip.indexOf(origin);
+        var destIndex = oneLineTrip.indexOf(destination);
+        var fullTrip = oneLineTrip.slice(origIndex, destIndex +1);
+      }
+      var fullTrip = oneLineTrip.slice(origIndex, destIndex +1);
+      console.log("Origin: " + origin);
+      console.log("Destination: " + destination);
+      console.log(fullTrip.join(" ---> "));
+      console.log((fullTrip.length - 1) + " stops total.");
+    }
+  }
 } else {
   // search through arrays for the origin platform
   for (var i = 0; i < lines.length; i++) {
     // if origin is in the array
     if (lines[i].indexOf(origin) !== -1) {
       // put the array in a variable
-      var start = lines[i];
-      var origIndex = start.indexOf(origin);
-      var interIndex = start.indexOf(intersection);
+      var startOfTrip = lines[i];
+      var origIndex = startOfTrip.indexOf(origin);
+      var interIndex = startOfTrip.indexOf(intersection);
       // if originIndex is larger than interIndex
       if (origIndex > interIndex) {
         // reverse order of array
-        start.reverse();
-        var origIndex = start.indexOf(origin);
-        var interIndex = start.indexOf(intersection);
-        var first = start.slice(origIndex, interIndex);
+        startOfTrip.reverse();
+        var origIndex = startOfTrip.indexOf(origin);
+        var interIndex = startOfTrip.indexOf(intersection);
+        var firstHalfTrip = startOfTrip.slice(origIndex, interIndex);
 
       }else {
 
-      var first = start.slice(origIndex, interIndex + 1);
+      var firstHalfTrip = startOfTrip.slice(origIndex, interIndex + 1);
       }
     }
   }
@@ -44,30 +57,25 @@ if (origin === intersection || destination === intersection) {
     // if destination is in the array
     if (lines[i].indexOf(destination) !== -1) {
       // put the array in a variable
-      var end = lines[i];
-      var destIndex = end.indexOf(destination);
-      var interIndex = end.indexOf(intersection);
+      var endOfTrip = lines[i];
+      var destIndex = endOfTrip.indexOf(destination);
+      var interIndex = endOfTrip.indexOf(intersection);
       // if destinIndex is smaller than interIndex
       if (destIndex < interIndex) {
         // reverse order of array
-        end.reverse();
-        var destIndex = end.indexOf(destination);
-        var interIndex = end.indexOf(intersection);
-        var last = end.slice(interIndex, destIndex +1);
+        endOfTrip.reverse();
+        var destIndex = endOfTrip.indexOf(destination);
+        var interIndex = endOfTrip.indexOf(intersection);
+        var lastHalfTrip = endOfTrip.slice(interIndex, destIndex +1);
       }else {
-      var last = end.slice(interIndex, destIndex +1);
+      var lastHalfTrip = endOfTrip.slice(interIndex, destIndex +1);
 
       }
     }
   }
-  var journey = first.concat(last);
-  console.log(journey);
+  var fullTrip = firstHalfTrip.concat(lastHalfTrip);
+  console.log("Origin: " + origin);
+  console.log("Destination: " + destination);
+  console.log(fullTrip.join(" ---> "));
+  console.log((fullTrip.length - 1) + " stops total.");
 }
-
-
-
-
-
-
-// make an if statement that says if origin and destination are in the same array, then do separate thing.
-// if origIndex is greater than interIndex or destIndex then reverse array, and after that concat the array.
