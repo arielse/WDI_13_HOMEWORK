8368ed23520e81b620164c1a3f582942396d09c9
arielse <ariel@bottledup.net>
2017-09-11 21:27:56 +1000

    made functions

--- Ariel Smith-Essers/wk02/5-fri/web_groups/app.js
+++ Ariel Smith-Essers/wk02/5-fri/web_groups/app.js
@@ -10,33 +10,68 @@ var button = document.querySelector('button');
 var groupOutput = document.querySelector('.group-output');
 
 
+// var data = {
+//   numberOfGroups: 0;
+//   groupSize: 0;
+//   things: []
+// }
+
+// var foo = function() {
+//   // fetch the stuff from the DOM
+//   data.groupSize = 10;
+//
+//   bar(groupSize, numberOfGroups, things);
+// }
+
+
+
+var groupSizeFn = function() {
+  return Number(groupSizeInput.value);
+}
 
 // makes an array of strings from the input list
 var listArrMaker = function() {
   // gets the number value of group size
-  var groupSize = Number(groupSizeInput.value);
 
   var groupString = listInput.value;
   var groupArr = groupString.split(", ");
-  var length = groupArr.length;
+  return groupArr;
+}
+
+var shuffleArray = function(inputArray) {
+  var shuffledArray = [];
+
+  var length = inputArray.length;
   var groupArrShuffle = [];
 
   for (var i = 0; i < length; i++) {
-    arrShuffle = Math.floor(Math.random() * groupArr.length);
-    groupArrShuffle.push(groupArr[arrShuffle]);
-    groupArr.splice(arrShuffle, 1);
+    var arrShuffle = Math.floor(Math.random() * inputArray.length);
+    shuffledArray.push(inputArray[arrShuffle]);
+    inputArray.splice(arrShuffle, 1);
   }
 
-// gets the array and splits it depending on the groupSize entered.
-  for (var i = 0; i = groupArrShuffle.length/groupSize; i++) {
-    if (groupArrShuffle.length % groupSize === 1) {
-      var oddItem = groupArrShuffle.splice((groupArrShuffle.length - 1), 1);
-    }
-    console.log(groupArrShuffle.splice(0, groupSize));
+  return shuffledArray;
+}
+
+
+var showFinishedGroups = function(groupArr){
+
+  var groupArrShuffle = shuffleArray(groupArr);
+  // gets the array and splits it depending on the groupSize entered.
+  for (var i = 0; i <= groupArrShuffle.length/groupSizeFn(); i++) {
+    console.log(groupArrShuffle.splice(0, groupSizeFn()));
+
   };
+
+  console.log(groupArrShuffle);
 
   listInput.value = "";
   groupSizeInput.value = "";
+}
+
+var output = function() {
+  var origArr = listArrMaker();
+  showFinishedGroups(origArr);
 }
 
 
@@ -47,5 +82,5 @@ var listArrMaker = function() {
 
 
 button.addEventListener('click', function(){
-  listArrMaker();
+  output();
 });
