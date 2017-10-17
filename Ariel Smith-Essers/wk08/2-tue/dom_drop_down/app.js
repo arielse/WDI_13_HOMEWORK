9b6f12247dd5e1edc2053fede0e8b3e5d18067e7
arielse <ariel@bottledup.net>
2017-10-18 10:26:23 +1100

    trying to get image functionality

--- Ariel Smith-Essers/wk08/2-tue/dom_drop_down/app.js
+++ Ariel Smith-Essers/wk08/2-tue/dom_drop_down/app.js
@@ -1,11 +1,16 @@
 var cityType = document.querySelector('#city-type');
 var cities = ['Austin', 'Los Angeles', 'New York City', 'San Francisco', 'Sydney'];
-
+var body = document.querySelector('body');
 
 cities.forEach(function(city) {
   var node = document.createElement('option');
   var textnode = document.createTextNode(city);
   node.appendChild(textnode);
   cityType.appendChild(node);
+});
+var list = document.querySelectorAll('option');
 
+list[1].addEventListener('click', function() {
+  body.classList.add('austin');
+  console.log('austin clicked');
 });
