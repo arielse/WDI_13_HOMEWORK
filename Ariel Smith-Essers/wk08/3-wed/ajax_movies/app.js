2eaa69a8d75ed6b554492b570cd999bb42a1c157
arielse <ariel@bottledup.net>
2017-10-20 12:35:49 +1100

    ajax movies

--- Ariel Smith-Essers/wk08/3-wed/ajax_movies/app.js
+++ Ariel Smith-Essers/wk08/3-wed/ajax_movies/app.js
@@ -1,10 +1,23 @@
 var movie = document.querySelector('input');
 
-// var movieSearch = {
-//   dataType: "json",
-//   url: 'http://www.omdbapi.com/?apikey=2f6435d9&s='+movie.value,
-//   data: result
-// }
+$("button").click(function(){
+    $.getJSON('http://www.omdbapi.com/?apikey=2f6435d9&s='+movie.value, function(result){
+      result.Search.forEach(function(film) {
+        console.log(film.Title);
+        var $movieResults = $('#movie-results');
+        var $movieTitles = $('<h2>');
+        $movieTitles.append(film.Title);
+        $movieResults.append($movieTitles);
+      });
+    });
+});
+
+
+var movieSearch = {
+  dataType: "json",
+  url: 'http://www.omdbapi.com/',
+  data: {apikey: '2f6435d9', s: movie.value}
+}
 // var showTitles = function(result) {
 //   result.Search.forEach(function(film) {
 //   console.log(film.Title);
@@ -17,16 +30,3 @@ var movie = document.querySelector('input');
 // $("button").on(click, function() {
 //   $.ajax(movieSearch).done(showTitles);
 // });
-
-
-$("button").click(function(){
-    $.getJSON('http://www.omdbapi.com/?apikey=2f6435d9&s='+movie.value, function(result){
-      result.Search.forEach(function(film) {
-        console.log(film.Title);
-        var $movieResults = $('#movie-results');
-        var $movieTitles = $('<h2>');
-        $movieTitles.append(film.Title);
-        $movieResults.append($movieTitles);
-      });
-    });
-});
